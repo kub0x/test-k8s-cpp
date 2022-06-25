250a0de5793a63bb12f247c33c1afd4d62bafb69
Borja Gomez <borja.gomez@develrox.com>
2022-06-25 15:52:34 -0400

    sleep on example

--- main.cc
+++ main.cc
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+
 
 int main (int argc, char **argv){
 	std::cout << "Simple test" << std::endl;
-	std::getchar();
+	while (true){
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
 	return 0;
 }
