44690149d7171e1ecbfa205f09f28e7fe37a396f
Borja Gomez <borja.gomez@develrox.com>
2022-06-25 16:30:24 -0400

    example v2

--- main.cc
+++ main.cc
@@ -4,7 +4,7 @@
 
 
 int main (int argc, char **argv){
-	std::cout << "Simple test" << std::endl;
+	std::cout << "Simple test version 2" << std::endl;
 	while (true){
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
